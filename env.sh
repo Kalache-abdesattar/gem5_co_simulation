3c5ec6ece7ebdbae92cfbb7f652628ea3d95ff5e
dmk <abdesattar.kalache@tuni.fi>
2025-10-26 17:36:40 +0000

    updated env.sh

--- env.sh
+++ env.sh
@@ -5,13 +5,13 @@ cd gem5/
 ## X86: build .fast and .opt version
 
 #// scons build/X86_CHI/gem5.debug -j$(nproc)
-scons build/X86_CHI/gem5.fast -j$(nproc)
+# scons build/X86_CHI/gem5.fast -j$(nproc)
 scons build/X86_CHI/gem5.opt -j$(nproc)
 
 ## same for RISCV
 
 #// scons build/RISCV_CHI/gem5.debug -j$(nproc)
-scons build/RISCV_CHI/gem5.fast -j$(nproc)
+# scons build/RISCV_CHI/gem5.fast -j$(nproc)
 scons build/RISCV_CHI/gem5.opt -j$(nproc)
 
 cd -
@@ -23,13 +23,12 @@ mkdir -p images/bootloader
 mkdir -p images/disk
 
 
-## riscv-ubuntu-24.04 and x86-ubuntu-22.04 disk images
-
-#wget 
-#wget 
 
 ## bootloaders/kernels
-wget -P images/kernel https://gem5dist.blob.core.windows.net/dist/develop/kernels/riscv/static/linux-6.6.33
-wget -P images/kernel https://gem5dist.blob.core.windows.net/dist/develop/kernels/x86/static/vmlinux-x86-ubuntu-6.8.0-52-generic
+wget -O images/kernel/riscv-linux-5.15.180 https://gem5dist.blob.core.windows.net/dist/develop/kernels/riscv/static/riscv-linux-5.15.180-kernel
+wget -O images/kernel/x86-linux-6.8.0-52 https://gem5dist.blob.core.windows.net/dist/develop/kernels/x86/static/vmlinux-x86-ubuntu-6.8.0-52-generic
 
-wget -P images/bootloader https://gem5dist.blob.core.windows.net/dist/develop/kernels/riscv/static/riscv-bootloader-opensbi-1.3.1-20231129
+wget -O images/bootloader/riscv-bootloader-opensbi-1.3.1 https://gem5dist.blob.core.windows.net/dist/develop/kernels/riscv/static/riscv-bootloader-opensbi-1.3.1-20231129
+
+mkdir -p checkpoints
+mkdir -p out
